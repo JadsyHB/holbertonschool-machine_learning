1b01a8898564de624a2e3671901c07878f0727fc
JadsyHB <3219@holbertonschool.com>
2021-07-22 18:12:17 +0000

    fix

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """summation square"""
 
+
 def summation_i_squared(n):
     """
     returns sum squared
     """
-    if n:
+    try:
         return int((n*(n+1)*(2*n+1))/6)
-    return None
+    except:
+        return None
