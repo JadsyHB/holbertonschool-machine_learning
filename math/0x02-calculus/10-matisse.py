983573e6a963297e9713c87958bc1adeab8cb115
JadsyHB <3219@holbertonschool.com>
2021-07-22 18:26:56 +0000

    m

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -4,14 +4,14 @@
 
 def poly_derivative(poly):
     """poly deriv returns a list of coeffs"""
-     if type(poly) is not list or len(poly) < 1:
-         return None
-     for i in poly:
-         if type(i) is not int and type(i) is not float:
-             return None
+    if type(poly) is not list or len(poly) < 1:
+        return None
+    for i in poly:
+        if type(i) is not int and type(i) is not float:
+            return None
     deriv = []
     for i in range(len(poly)):
         deriv.append(i * poly[i])
     while deriv[-1] is 0 and len(deriv) > 1:
         deriv = deriv[:-1]
-    return derivative
+    return deriv[1:]
