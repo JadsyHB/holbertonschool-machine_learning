8b3f4749fb5241b996cf03943a68d64e9e520703
JadsyHB <3219@holbertonschool.com>
2021-08-08 17:21:53 +0000

    pep8

--- supervised_learning/0x01-classification/5-neuron.py
+++ supervised_learning/0x01-classification/5-neuron.py
@@ -49,7 +49,7 @@ class Neuron:
     def cost(self, Y, A):
         """returns the cost"""
         m = Y.shape[1]
-        c = (-1 / m) * np.sum((Y * np.log(A)) + (1 - Y) * np.log(1.0000001 - A))
+        c = - np.sum((Y * np.log(A)) + (1 - Y) * np.log(1.0000001 - A)) / m
         return c
 
     def evaluate(self, X, Y):
