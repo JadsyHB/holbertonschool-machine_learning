0b398aee9b21c97f9302e5733a57872d356941c3
JadsyHB <3219@holbertonschool.com>
2021-08-10 23:53:53 +0300

    added deep

---
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+deep neural network
+"""
+
+
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """
+    deep neural network class
+    """
+
+    def __init__(self, nx, layers):
+        """
+        initialization
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(layers) is not list or len(layers) < 1:
+            raise TypeError("layers must be a list of positive integers")
+        weights = {}
+        pv = nx
+        for i in range(len(layers)):
+            if type(layers[i]) is not int or layers[i] < 0:
+                raise TypeError("layers must be a list of positive integers")
+            weights["b{}".format(i + 1)] = np.zeros((layers[i], 1))
+            weights["W{}".format(i + 1)] = (
+                np.random.randn(layers[i], pv) * np.sqrt(2 / pv)
+            )
+            pv = layers[i]
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = weights
---
+++ supervised_learning/0x01-classification/18-deep_neural_network.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""
+deep neural network
+"""
+
+
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """
+    deep neural network class
+    """
+
+    def __init__(self, nx, layers):
+        """
+        initialization
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(layers) is not list or len(layers) < 1:
+            raise TypeError("layers must be a list of positive integers")
+        weights = {}
+        pv = nx
+        for i in range(len(layers)):
+            if type(layers[i]) is not int or layers[i] < 0:
+                raise TypeError("layers must be a list of positive integers")
+            weights["b{}".format(i + 1)] = np.zeros((layers[i], 1))
+            weights["W{}".format(i + 1)] = (
+                np.random.randn(layers[i], pv) * np.sqrt(2 / pv)
+            )
+            pv = layers[i]
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = weights
+
+    @property
+    def L(self):
+        """getter for L"""
+        return self.__L
+
+    @property
+    def cache(self):
+        """getter for cache"""
+        return self.__cache
+
+    @property
+    def weights(self):
+        """getter for weights"""
+        return self.__weights
+
+    def forward_prop(self, X):
+        """forward prop"""
+        self.__cache["A0"] = X
+        for i in range(self.L):
+            W = self.weights["W{}".format(i + 1)]
+            b = self.weights["b{}".format(i + 1)]
+            fp = np.matmul(W, self.cache["A{}".format(i)]) + b
+            A = (1 / (1 + np.exp(-fp)))
+            self.__cache["A{}".format(i + 1)] = A
+        return (A, self.cache)
