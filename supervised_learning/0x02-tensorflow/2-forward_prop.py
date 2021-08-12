1cb550a8ecab9df39611861d88828368651fde60
JadsyHB <3219@holbertonschool.com>
2021-08-12 20:49:47 +0300

    added module 2

---
+++ supervised_learning/0x02-tensorflow/2-forward_prop.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+create placeholders module
+"""
+
+
+import tensorflow as tf
+
+
+def forward_prop(x, layer_sizes=[], activations=[]):
+    """returns the predicition in tensor form"""
+    create_layer = __import__('1-create_layer').create_layer
+    l = len(layer_sizes)
+    out = create_layer(x, layer_sizes[0], activations[0])
+    for i in range(1, l):
+        out = create_layer(out, layer_sizes[0], activations[0])
+    return out
