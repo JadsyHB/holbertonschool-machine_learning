ba81c964c296367b0d67f335e43859f04c167972
JadsyHB <3219@holbertonschool.com>
2021-08-14 22:19:53 +0300

    e

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -7,6 +7,9 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """returns one hot matrix"""
-    oh = np.zeros((Y.shape[0], classes))
-    oh[np.arange(Y.shape[0]), Y] = 1
+    if type(Y) is not np.ndarray:
+        return None
+    if type(classes) is not int:
+        return None
+    oh = np.eye(classes)[Y].transpose()
     return oh
