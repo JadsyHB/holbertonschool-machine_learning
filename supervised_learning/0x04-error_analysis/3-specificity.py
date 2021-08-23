124741c36544bfe4bd7a810c849ec73858bb91ed
JadsyHB <3219@holbertonschool.com>
2021-08-23 20:49:19 +0300

    init

--- supervised_learning/0x04-error_analysis/3-specificity.py
+++ supervised_learning/0x04-error_analysis/3-specificity.py
@@ -10,11 +10,6 @@ import numpy as np
 def specificity(confusion):
     """returns matrix with specificities"""
     classes = confusion.shape[0]
-    spec = [np.zeros(classes)]
-    total = sum(confusion)
-    for i in range(classes):
-        val = confusion[i, i]
-        fp = sum(confusion[:, i]) - val
-        tn = sum(total) - sum(confusion[:, i]) - sum(confusion[i, :]) + val
-        spec[i] = tn / (tn + fp)
-    return spec
+    spec = [np.delete(np.delete(confusion, i, 1), i, 0).sum(
+    ) / np.delete(confusion, i, 0).sum() for i in range(classes)]
+    return np.asarray(spec)
