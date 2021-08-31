d0cb33ca328b66729fd33291708f065d7c1d4552
JadsyHB <3219@holbertonschool.com>
2021-08-31 16:59:57 +0300

    fix pep8

--- supervised_learning/0x06-keras/4-train.py
+++ supervised_learning/0x06-keras/4-train.py
@@ -5,7 +5,8 @@
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size, epochs, verbose=True, shuffle=False):
+def train_model(network, data, labels, batch_size, epochs,
+                verbose=True, shuffle=False):
     """returns the history"""
     hist = network.fit(data, labels, epochs=epochs,
                        batch_size=batch_size, verbose=verbose,
--- supervised_learning/0x06-keras/5-train.py
+++ supervised_learning/0x06-keras/5-train.py
@@ -5,7 +5,8 @@
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size, epochs, validation_data=None, verbose=True, shuffle=False):
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, verbose=True, shuffle=False):
     """validates data or None"""
     val = network.fit(x=data, y=labels, batch_size=batch_size,
                       epochs=epochs,
