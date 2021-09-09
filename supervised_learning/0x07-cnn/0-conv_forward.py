ef08644013f0e3737b4ef58d87e689ed30b6aaca
JadsyHB <3219@holbertonschool.com>
2021-09-09 20:33:52 +0300

    init

---
+++ supervised_learning/0x07-cnn/0-conv_forward.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""convolutional forward prop"""
+
+
+import numpy as np
+
+
+def conv_forward(A_prev, W, b, activation, padding='same', stride=(1, 1)):
+    """Returns: the output of the convolutional layer"""
+    m, h_prev, w_prev, c_prev = A_prev.shape
+    kh, kw, c_prev, c_new = W.shape
+    sh, sw = stride
+    if padding == 'same':
+        ph = int(np.ceil((((h_prev - 1) * sh + kh - h_prev) / 2)))
+        pw = int(np.ceil((((w_prev - 1) * sw + kw - w_prev) / 2)))
+    pw, ph = 0
+    img_pad = np.pad(A_prev,
+                     pad_width=((0, 0), (ph, ph), (pw, pw), (0, 0)),
+                     mode='constant')
+    c_h = int(((h_prev + 2 * ph - kh) / sh) + 1)
+    c_w = int(((w_prev + 2 * pw - kw) / sw) + 1)
+    conv = np.zeros((m, c_h, c_w, c_new))
+    for i in range(c_h):
+        for j in range(c_w):
+            for k in range(c_new):
+                v_s = i * sh
+                v_e = v_s + kh
+                h_s = j * sw
+                h_e = h_s + kw
+                sliced = img_pad[:, v_s:v_e, h_s:h_e]
+                kernel = W[:, :, :, k]
+                conv[:, i, j, k] = np.sum(
+                    np.multiply(sliced, kernel), axis=(1, 2, 3))
+    return activation(conv + b)
---
+++ supervised_learning/0x07-cnn/1-pool_forward.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""convolutional forward prop"""
+
+
+import numpy as np
+
+
+def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
+    """Returns: the output of the pooling layer"""
+    m, h_prev, w_prev, c_prev = A_prev.shape
+    kh, kw = kernel_shape
+    sh, sw = stride
+    c_h = int((h_prev - kh) / sh) + 1
+    c_w = int((w_prev - kw) / sw) + 1
+
+    conv = np.zeros((m, c_h, c_w, c_prev))
+    for i in range(c_h):
+        for j in range(c_w):
+            if mode == "max":
+                conv[:, i, j] = np.max(
+                    A_prev[:, i * sh:((i * sh) + kh), j * sw:((j * sw) + kw)], axis=(1, 2))
+            elif mode == "avg":
+                conv[:, i, j] = np.mean(
+                    A_prev[:, i * sh:((i * sh) + kh), j * sw:((j * sw) + kw)], axis=(1, 2))
+    return conv
